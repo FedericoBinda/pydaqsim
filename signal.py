a2a23164e6a9a46309c51ce34b2b78b35e34af2f
Federico Binda <federico.binda@physics.uu.se>
2015-12-03 17:18:15 +0100

    removed non fundamental variables from signal class

--- signal.py
+++ signal.py
@@ -4,17 +4,6 @@ class signal:
     '''signal class'''
     def __init__(self,ID=None,Type=None,arrival_time=None):
         self._ID = ID
-        self.amplitude = []
-        self.timevector = []
         self._type = Type
         self._arrival_time = arrival_time
-        self.ampunit = None
-        self.timeunit = None
-
-    def SetAmpUnit(self,unit):
-        allowed = ['V','a.u.']
-        if unit in allowed:
-            self.ampunit = unit
-        else:
-            print 'ERROR! unit not allowed!'
-        
+        self.amplitude = []
